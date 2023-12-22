bc655d72da0cb120705ff54282f4d0965b277f79
DURGA VENU KUMAR MUTYALA <145457524+thevk22@users.noreply.github.com>
2023-12-22 11:54:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -124,4 +124,10 @@ if predict_button:
             for feature, importance in feature_importances.head(5).iterrows():
                 st.write(f"- **{feature}:** {importance['Importance']:.4f}")
 
-
+hide_streamlit_style = """
+            <style>
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            footer {visibility: hidden !important;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
